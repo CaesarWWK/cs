a2eb6ad9aa402cba21d4365420c1ce479afbe739
Weikuan <wk632931@dal.ca>
2017-05-09 12:21:08 -0300

    233

---
+++ CS3130-Demo-master-1/app/src/main/java/com/acme/csci3130/MainActivity.java
@@ -0,0 +1,34 @@
+package com.acme.csci3130;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.content.Intent;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+
+
+
+
+    public void sendMessage(View view) {
+        EditText editText = (EditText) findViewById(R.id.editText);
+        String message = editText.getText().toString();
+        TextView hello = (TextView) findViewById(R.id.helloText);
+        hello.setText(message);
+
+
+    }
+}
